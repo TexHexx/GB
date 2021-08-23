00e1de66d70a8ef244634c41ec048ceae0ccdbd3
TexHexx <prior_it@mail.ru>
2021-08-23 19:27:38 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+
+class Power
+{
+private:
+    int a = 0, b = 0;
+public:
+    void set(int a, int b) 
+    {
+        this->a = a;
+        this->b = b;
+    };
+    int calculate()
+    {
+        return pow(a, b);
+    }
+};
+
+int main()
+{
+    Power pw;
+    pw.set(15, 20);
+    std::cout << "Return number: " << pw.calculate();
+}
